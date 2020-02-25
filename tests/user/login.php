c2a7d72a1f0ba0e8de769b645c7e1365a3675e04
xulihui <lihuixu@basicfinder.com>
2020-02-25 14:17:27 +0800

    调整账户信息

--- tests/user/login.php
+++ tests/user/login.php
@@ -17,7 +17,14 @@ use BasicfinderSaas\SaasApi;
 
 $saasapi = new SaasApi();
 
-$account = require(__DIR__.'/../config/account.php');
+$account = [
+    'username' => '1234566@qq.com',
+    'password' => 'bf123456',
+    'app_key' => 'pc-passport',
+    'app_version' => '1.0.0',
+    'device_name' => 'Win32',
+    'device_number' => '123456'
+];
 $result = $saasapi->user->login($account);
 
 if (!empty($result['error']))
